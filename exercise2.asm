af59c16dfd0069d48aa88381d49e6a62d3803831
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2023-05-20 13:34:50 +0300

    Update exercise2.asm

--- exercise2.asm
+++ exercise2.asm
@@ -1,6 +1,6 @@
 .data 
-q4:			.asciiz "first i=1 if(a[i]>10) i=i+1 last i= "
-a:			.word 12, 2, 3, 4, 5, 6, 7, 8, 9
+q4:		.asciiz "first i=1 if(a[i]>10) i=i+1 last i= "
+a:		.word 12, 2, 3, 4, 5, 6, 7, 8, 9
 index:		.word 1
 
 .text
@@ -26,6 +26,10 @@ END:
 
 	li $v0, 1
 	move $a0, $s1
-	syscall					
+	syscall		
+	
+	li $v0, 10				# terminate the program
+	syscall
+	
 	
 
